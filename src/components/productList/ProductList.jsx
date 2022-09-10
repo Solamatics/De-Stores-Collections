113acd0a1729ba3ac4ae962cecff81b787d44544
Olusola Afikode <afikodeolaitan@gmail.com>
2022-09-10 15:12:19 +0100

    create product components

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import data from "../../data.json";
+import Products from "../products/Products";
+import "./productList.css";
 
-const ProductList = () => {
-  return <div>ProductList</div>;
-};
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <div className="content">
+          <div className="main">
+            <Products products={this.state.products} />
+          </div>
+          <div className="sidebar">Cart Items</div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default ProductList;
