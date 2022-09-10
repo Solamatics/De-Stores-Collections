954b0c64a857dc1d1b7c99fd251da7c87f106c67
Olusola Afikode <afikodeolaitan@gmail.com>
2022-09-10 20:01:06 +0100

    implemented product cart section

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -16,7 +16,12 @@ export default class Products extends Component {
                 </a>
                 <div className="product__price">
                   <div>{formatCurrency(product.price)}</div>
-                  <button className="button primary">Add To Cart</button>
+                  <button
+                    onClick={() => this.props.addToCart(product)}
+                    className="button primary"
+                  >
+                    Add To Cart
+                  </button>
                 </div>
               </div>
             </li>
